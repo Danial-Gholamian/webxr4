6ca49d473c1ea2defd1e7bb8f89c2e9efec077bf
Fatimah Ibrahim <fatimahibrahim2004@gmail.com>
2026-04-20 01:27:26 +0200

    added basic task questions

--- questionList.js
+++ questionList.js
@@ -1,11 +1,38 @@
 export const QUESTIONS = [
+        {
+        question: "Set the temporal window size to approximately 150 units and move it to the interval [4300–4450]. Set the interaction mode to Inter-group. Which group has the highest number of interactions in this interval?",
+        answers: [
+            "Teachers", // Correct
+            "Group 1B",
+            "Group 4A",
+            "Group 5B"
+        ]
+    },
+    {
+        question: "Reset the graph. Expand the temporal window to cover the entire Day 2. Filter the graph to Group 4A and locate node 1546. Set the interaction mode to Intra-group. How many interactions does this node have with its group?",
+        answers: [
+            "16", // Correct
+            "12",
+            "20",
+            "8"
+        ]
+    },
+    {
+        question: "Reduce the temporal window size to a single timestamp and move it to the interval [1554–1556]. How many active nodes are visible in the graph?",
+        answers: [
+            "22", // Correct
+            "18",
+            "25",
+            "30"
+        ]
+    },
     {
         question: "Adjust the temporal window to approximately match the width of a histogram bar (≈100 units). Which interval shows the highest interaction activity, and which three groups are the most active?",
         answers: [
-            "Interval A — Groups 1B, 3B, 4B", // Correct Answer
-            "Interval B — Groups 4B, Teachers, 5A",
-            "Interval C — Groups 1B, 2B, 3A", 
-            "Interval D - Groups 5B, 2A, 1B"
+            "Groups 1B, 3B, 4B", // Correct Answer
+            "Groups 4B, Teachers, 5A",
+            "Groups 1B, 2B, 3A", 
+            "Groups 5B, 2A, 1B"
 
         ]
     },
